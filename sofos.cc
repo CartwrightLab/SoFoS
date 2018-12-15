6b4184e25900003c0f473f22a540e508c4f80d9b
Reed A. Cartwright <cartwright@asu.edu>
2018-12-15 00:53:10 -0700

    Add more features
    - Add folded spectrum estimates
    - Make folded default to ref/alt
    - Use bcf_calc_ac to calculate allele frequences
    - Make unfoled default to AA tag
    - Add ability to included zero alleles
    - Don't worry about missing reference alleles
    - Don't worry about sites with no calls
    - Add progress indicator
    - Refactor code

--- sofos.cc
+++ sofos.cc
@@ -28,10 +28,12 @@ SOFTWARE.
 #include <cmath>
 #include <limits>
 #include <chrono>
+#include <numeric>
 
 #include <unistd.h>
 
 #include <htslib/vcf.h>
+#include <htslib/vcfutils.h>
 
 void print_usage(const char* exe, std::ostream& os) {
     os << "Usage: " << exe << " -a alpha_param -b beta_param -n rescale_size [filepath] > output.csv\n";
@@ -39,9 +41,12 @@ void print_usage(const char* exe, std::ostream& os) {
 
 constexpr unsigned int SOFOS_FLAG_DEFAULT=0;
 constexpr unsigned int SOFOS_FLAG_UNFOLDED=1;
-constexpr unsigned int SOFOS_FLAG_ALTREF=2;
+constexpr unsigned int SOFOS_FLAG_REFALT=2;
 
-int sofos_main(const char *path, double alpha, double beta, int size, unsigned int flags=SOFOS_FLAG_DEFAULT);
+bool g_sofos_quiet=false;
+
+int sofos_main(const char *path, double alpha, double beta, int size,
+	double zero, unsigned int flags=SOFOS_FLAG_DEFAULT);
 void print_usage(const char* exe, std::ostream& os);
 
 struct buffer_free_t {
@@ -72,16 +77,16 @@ struct bcf_free_t {
 template<typename T>
 using buffer_t = std::unique_ptr<T[],buffer_free_t>;
 
-int get_genotypes(const bcf_hdr_t *header, bcf1_t *record,
-    buffer_t<int32_t>* buffer, int *capacity);
 int get_string(const bcf_hdr_t *header, bcf1_t *record,
     const char *tag, buffer_t<char>* buffer, int *capacity);
 
 std::pair<std::string, std::string> timestamp();
 bool is_ref_missing(const bcf1_t* record);
 bool is_allele_missing(const char* a);
-void update_counts(double a, double b, std::vector<double> *counts);
-void update_bins(double a, double b, std::vector<double> *counts);
+void update_counts(double a, double b, double weight, std::vector<double> *counts);
+void update_bins(double a, double b, double weight, std::vector<double> *counts);
+
+void fold_histogram(std::vector<double> *counts);
 
 template<typename T>
 inline
@@ -98,10 +103,11 @@ int main(int argc, char *argv[]) {
         char c = 0;
         double alpha = 1.0;
         double beta = 1.0;
+        double zero = 0.0;
         int size = 9;
-        bool unfolded = false;
-        bool altref = false;
-        while((c = getopt(argc, argv, "a:b:n:hufr")) != -1) {
+        bool folded = true;
+        int REFALT = -1;
+        while((c = getopt(argc, argv, "a:b:n:hufrtz:qv")) != -1) {
             switch(c) {
             case 'a':
                 alpha = std::stod(optarg);
@@ -112,17 +118,27 @@ int main(int argc, char *argv[]) {
             case 'n':
                 size =  std::stoi(optarg);
                 break;
+            case 'z':
+            	zero = std::stod(optarg);
+            	break;
             case 'u':
-                unfolded = true;
-                break;
-            case 'r':
-                altref = true;
-                unfolded = true;
+                folded = false;
                 break;
             case 'f':
-                unfolded = false;
-                altref = false;
+                folded = true;
                 break;
+            case 'r':
+                REFALT = 1;
+                break;
+            case 't':
+                REFALT = 0;
+                break;
+            case 'q':
+            	g_sofos_quiet = true;
+            	break;
+            case 'v':
+            	g_sofos_quiet = false;
+            	break;
             case 'h':
                 print_usage(argv[0], std::cout);
                 return EXIT_SUCCESS;
@@ -140,13 +156,9 @@ int main(int argc, char *argv[]) {
             throw std::invalid_argument("more than one input file was specified.");
         }
         unsigned int flags = SOFOS_FLAG_DEFAULT;
-        if(unfolded) {
-            flags |= SOFOS_FLAG_UNFOLDED;
-            if(altref) {
-                flags |= SOFOS_FLAG_ALTREF;
-            }
-        }
-        return sofos_main(path, alpha, beta, size,flags);
+        flags |= (!folded) ? SOFOS_FLAG_UNFOLDED : 0; 
+     	flags |= (REFALT == 1 || (REFALT != 0 && folded)) ? SOFOS_FLAG_REFALT : 0;
+        return sofos_main(path, alpha, beta, size, zero, flags);
 
     } catch(std::exception &e) {
         std::cerr << "ERROR: " << e.what() << std::endl;
@@ -154,7 +166,7 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
 }
 
-int sofos_main(const char *path, double alpha, double beta, int size, unsigned int flags) {
+int sofos_main(const char *path, double alpha, double beta, int size, double zero, unsigned int flags) {
     assert(path != nullptr);
     vcfFile *input_ptr = vcf_open(path,"r");
     if(input_ptr == nullptr) {
@@ -171,17 +183,18 @@ int sofos_main(const char *path, double alpha, double beta, int size, unsigned i
     std::cout << "#alpha=" << alpha << "\n";
     std::cout << "#beta=" << beta << "\n";
     std::cout << "#size=" << size << "\n";
-
-    // spacer
-    std::cout << "#\n";
+    std::cout << "#folded=" << ((flags & SOFOS_FLAG_UNFOLDED) ? 0 : 1) << "\n";
+    std::cout << "#refalt=" << ((flags & SOFOS_FLAG_REFALT) ? 1 : 0) << "\n";
 
     std::unique_ptr<vcfFile,file_free_t> input{input_ptr};
     std::unique_ptr<bcf_hdr_t,header_free_t> header{bcf_hdr_read(input.get())};
     std::unique_ptr<bcf1_t,bcf_free_t> record{bcf_init()};
     
     size_t nsamples = bcf_hdr_nsamples(header.get());
-    int int_capacity = nsamples*4;
-    auto int_buffer = make_buffer<int32_t>(int_capacity);
+    if(nsamples <= 0) {
+    	throw std::invalid_argument("input vcf has no samples.");
+    }
+
     int char_capacity = 64;
     auto char_buffer = make_buffer<char>(char_capacity);
 
@@ -191,15 +204,23 @@ int sofos_main(const char *path, double alpha, double beta, int size, unsigned i
 
     size_t nsites = 0;
 
+    std::vector<int> ac_buffer;
+    int ac_which = BCF_UN_FMT;
+    // if AN and AC tags are defined, use them
+	if(bcf_hdr_id2int(header.get(), BCF_DT_ID, "AN") >= 0 && 
+	   bcf_hdr_id2int(header.get(), BCF_DT_ID, "AC") >= 0 ) {
+		ac_which |= BCF_UN_INFO; 
+	}
+
+    auto start = std::chrono::steady_clock::now();
+    auto last = start;
+
     while(bcf_read(input.get(), header.get(), record.get()) == 0) {
         bcf_unpack(record.get(), BCF_UN_STR);
         //std::cerr << record.get()->rid << ":" << record.get()->pos << std::endl;
-        if(is_ref_missing(record.get())) {
-            continue;
-        }
         // identify which allele is ancestral
         int anc = 0;
-        if(!(flags & SOFOS_FLAG_ALTREF)) {
+        if(!(flags & SOFOS_FLAG_REFALT)) {
             int n = get_string(header.get(), record.get(), "AA", &char_buffer, &char_capacity);
             if(n <= 0) {
                 // missing, unknown, or empty tag
@@ -211,45 +232,79 @@ int sofos_main(const char *path, double alpha, double beta, int size, unsigned i
                 }
             }
         }
-        int n_ref = 0;
-        int n_alt = 0;
 
-        int n = get_genotypes(header.get(), record.get(), &int_buffer, &int_capacity);
-        int max_ploidy = n/nsamples;
-        for(int sample = 0; sample < nsamples; ++sample) {
-            int32_t *gt = int_buffer.get()+sample*max_ploidy;
-            for(int i=0; i < max_ploidy; ++i) {
-                if(gt[i] == bcf_int32_vector_end) {
-                    break;
-                }
-                if(bcf_gt_is_missing(gt[i])) {
-                    continue;
-                }
-                int allele = bcf_gt_allele(gt[i]);
-                n_ref += (allele == anc);
-                n_alt += (allele != anc);
-            }
+        ac_buffer.assign(record.get()->n_allele,0.0);
+        if(bcf_calc_ac(header.get(), record.get(), ac_buffer.data(),
+        		ac_which) == 0) {
+        	continue;
         }
-        int n_total = n_ref+n_alt;
+        int n_total = std::accumulate(ac_buffer.begin(), ac_buffer.end(), 0);
         if(n_total == 0) {
             continue;
         }
-        update_counts(alpha + n_alt, beta + n_ref, &posterior);            
+        int n_anc = (anc < ac_buffer.size()) ? ac_buffer[anc] : 0;
+        int n_der = n_total - n_anc;
 
-        update_bins(n_alt, n_ref, &bins);
+        if(flags & SOFOS_FLAG_UNFOLDED) {
+        	update_counts(alpha + n_der, beta + n_anc, 1.0, &posterior);    
+        } else {
+        	// When calculating the folded spectrum, we have to consider whether
+        	// each of our alleles is ancestral. 
+        	// The probability that an allele is the ancestral allele is its frequency
+        	double f = (1.0*n_der)/n_total;
+        	// der is ancestral
+        	update_counts(alpha + n_anc, beta + n_der, f, &posterior);
+        	// anc is ancestral
+        	update_counts(alpha + n_der, beta + n_anc, 1.0-f, &posterior);
+        }
+
+        if(n_der+n_anc > 0) {
+        	update_bins(n_der, n_anc, 1.0, &bins);
+        }
 
         nsites += 1;
+        if(!g_sofos_quiet && (nsites % 1024) == 0) {
+        	auto now = std::chrono::steady_clock::now();
+			auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(now - last);
+			if(elapsed.count() >= 60) {
+				std::cerr << "## [" << elapsed.count() << "s elapsed] "
+				          << nsites << " sites processed --- "
+				          << bcf_seqname(header.get(), record.get()) << ":" << record->pos+1
+				          << std::endl;
+			}
+        }
     }
+    if(!g_sofos_quiet) {
+    	auto now = std::chrono::steady_clock::now();
+		auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(now - last);
+		std::cerr << "## [" << elapsed.count() << "s elapsed] "
+			      << nsites << " sites processed"
+			      << std::endl;
+	}
+
+    // Add zero-count sites (for controlling ascertainment bias)
+    update_counts(alpha, beta+nsamples, zero, &posterior);
+    update_bins(0, nsamples, zero, &bins);
+
     // calculate prior
     std::vector<double> prior(posterior.size(), 0.0);
-    update_counts(alpha, beta, &prior);
+    update_counts(alpha, beta, nsites+zero, &prior);
+
+    if(!(flags & SOFOS_FLAG_UNFOLDED)) {
+    	fold_histogram(&prior);
+    	fold_histogram(&bins);
+    	fold_histogram(&posterior);
+    }
+
+    // spacer
+    std::cout << "#\n";
 
     // output resulting scale
     std::cout << std::setprecision(std::numeric_limits<double>::max_digits10);
     std::cout << "Number,Prior,Observed,Posterior\n";
-    for(int i=0;i<=size;++i) {
+    for(int i=0;i<posterior.size();++i) {
         std::cout << i
-                  << "," << nsites*prior[i]
+                  << "," << prior[i]
                   << "," << bins[i]
                   << "," << posterior[i]
                   << "\n";
@@ -257,22 +312,6 @@ int sofos_main(const char *path, double alpha, double beta, int size, unsigned i
     std::cout << std::flush;
     
     return EXIT_SUCCESS;
-}
-
-inline
-int get_genotypes(const bcf_hdr_t *header, bcf1_t *record,
-    buffer_t<int32_t>* buffer, int *capacity)
-{
-    int *p = buffer->get();
-    int n = bcf_get_genotypes(header, record, &p, capacity);
-    if(n == -4) {
-        throw std::bad_alloc{};
-    } else if(p != buffer->get()) {
-        // update pointer
-        buffer->release();
-        buffer->reset(p);
-    }
-    return n;
 }
 
 inline
@@ -313,8 +352,6 @@ bool is_ref_missing(const bcf1_t* record) {
     return is_allele_missing(record->d.allele[0]);
 }
 
-
-
 std::pair<std::string, std::string> timestamp() {
     using namespace std;
     using namespace std::chrono;
@@ -329,8 +366,9 @@ std::pair<std::string, std::string> timestamp() {
     return {buffer, to_string(epoch.count())};
 }
 
-void update_counts(double a, double b, std::vector<double> *counts) {
+void update_counts(double a, double b, double weight, std::vector<double> *counts) {
     assert(counts != nullptr);
+    assert(a > 0.0 && b > 0.0);
     using std::lgamma;
     double ab = a+b;
     int size = counts->size()-1;
@@ -340,12 +378,14 @@ void update_counts(double a, double b, std::vector<double> *counts) {
         double d = lgamma(a+k)+lgamma(b+size-k);
         d -= lgamma(k+1) + lgamma(size-k+1);
         d -= scale;
-        (*counts)[k] += exp(d);
+        (*counts)[k] += weight*exp(d);
     }
 }
 
-void update_bins(double a, double b, std::vector<double> *bins) {
+void update_bins(double a, double b, double weight, std::vector<double> *bins) {
     assert(bins != nullptr);
+    assert(a >= 0.0 && b >= 0.0);
+    assert(a+b > 0);
     // bins are from [0,n]
     int n = bins->size()-1;
     double f = a/(a+b)*n;
@@ -353,7 +393,15 @@ void update_bins(double a, double b, std::vector<double> *bins) {
     if(k > n) {
         k = n;
     }
-    (*bins)[k] += 1;
+    (*bins)[k] += weight;
 }
 
 
+inline
+void fold_histogram(std::vector<double> *counts) {
+	assert(counts != nullptr);
+	for(int k=0;k<counts->size()/2;++k) {
+		(*counts)[k] += (*counts)[counts->size()-k-1];
+	}
+	counts->resize((counts->size()+1)/2);
+}
